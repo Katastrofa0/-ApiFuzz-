ff083b59bb91503260faea3131f7f762dae4fe6a
katastrofa <119258885+Katastrofa0@users.noreply.github.com>
2023-08-23 18:42:08 +0300

    ApiFuzz.py

--- ApiFuzz.py
+++ ApiFuzz.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
 import aiohttp
+from urllib.parse import urljoin
 import sys
 from colorama import Fore, Style
 
@@ -17,13 +18,19 @@ print(Fore.RED + r"""
 """ + Style.RESET_ALL)
 
 async def fetch_word(session, url, word):
-    async with session.get(f"{url}/{word}") as resp:
+    full_url = urljoin(url, word)
+    async with session.get(full_url) as resp:
         if resp.status == 200:
             print("Status Code:", Fore.GREEN + "\u27A4", resp.status, Style.RESET_ALL)
-            data = await resp.json()
-            print("Word:", Fore.RED + "\u27A4", word, Style.RESET_ALL)
-            print("Received Data:")
-            print(data)
+            try:
+                data = await resp.json()
+                print("Word:", Fore.RED + "\u27A4", word, Style.RESET_ALL)
+                print("Received Data:")
+                print(data)
+            except aiohttp.client_exceptions.ContentTypeError:
+                print("Word:", Fore.RED + "\u27A4", word, Style.RESET_ALL)
+                print("Non-JSON Response")
+
 
 async def loop(url, wordlist):
     async with aiohttp.ClientSession() as session:
@@ -37,13 +44,16 @@ async def loop(url, wordlist):
         await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Api Fuzzer")
-    parser.add_argument("-url", type=str, help="UrL FoR ReqUEsT")
-    parser.add_argument("-wordlist", type=str, help="WoRdLiSt fOr ATtaCK")
-    args = parser.parse_args()
-    if not (args.url and args.wordlist):
-        print("You NeeD tO ProViDe aN *uRl* ANd *WorDlIsT*")
-        exit(1)
+    try:
+        parser = argparse.ArgumentParser(description="Api Fuzzer")
+        parser.add_argument("-url", type=str, help="UrL FoR ReqUEsT")
+        parser.add_argument("-wordlist", type=str, help="WoRdLiSt fOr ATtaCK")
+        args = parser.parse_args()
+        if not (args.url and args.wordlist):
+            print("You NeeD tO ProViDe aN *uRl* ANd *WorDlIsT*")
+            exit(1)
 
-    loop_coroutine = loop(args.url,args.wordlist)
-    asyncio.run(loop_coroutine)
+        loop_coroutine = loop(args.url, args.wordlist)
+        asyncio.run(loop_coroutine)
+    except (KeyboardInterrupt, asyncio.CancelledError):
+        print("Program was interrupted. Exiting...")
